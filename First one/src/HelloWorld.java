26372e105b2a21bd75832e009561d4ada016ae02
Chacko <jchacko@cslab21.cs.skidmore.edu>
2014-09-03 20:06:17 -0400

    Initial comit.

---
+++ First one/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author jchacko
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("\"Let's get dinner,\" he said.");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
