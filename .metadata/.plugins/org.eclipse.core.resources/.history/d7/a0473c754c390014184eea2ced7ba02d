d698793fa30edfc604c7c87afb959e2cbed4eb93
Chacko <jchacko@cslab21.cs.skidmore.edu>
2014-09-10 20:59:11 -0400

    here we go

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d7/a0473c754c390014184eea2ced7ba02d
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author jchacko
+ *
+ */
+public class CCBA {;
+
+	/**
+	 * @param args
+	 * @see 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    double rate, month, amount;
+	   
+	    
+		    System.out.println("Lets see the damage this time...");
+			Scanner keyboard = new Scanner(System.in);
+			
+		    System.out.println("How much debt have you accumulated?");
+		    amount = keyboard.nextDouble();
+		    
+		    System.out.println("What's your interest rate?");
+		    rate = keyboard.nextDouble();
+		    rate = rate/100;
+		    
+		    System.out.println("How many months?");
+		    month = keyboard.nextInt();
+		    
+	       
+	   
+	    keyboard.close();
+	}
+}
+	       
+public static void lastmsg(String[] args) {
+	
+	System.out.println("For " + month + " months your acccumulated debt is " + amount + " . At a rate of " + rate + " will grow to " + (amount * Math.pow(1.0 + rate, month)));
+}
+
+
+
--- Project 1/src/CCBA.java
+++ Project 1/src/CCBA.java
@@ -12,7 +12,7 @@ public class CCBA {;
 
 	/**
 	 * @param args
-	 * @see 
+	 * @see http://stackoverflow.com/questions/19150336/compounding-interest-program-in-java
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -32,16 +32,35 @@ public class CCBA {;
 		    System.out.println("How many months?");
 		    month = keyboard.nextInt();
 		    
-	       
-	   
 	    keyboard.close();
 	}
 }
-	       
-public static void lastmsg(String[] args) {
+	public static void YoN(String[] args) {
+		/* I DID NOT COPY AND PASTE THIS!/* 
+		 * 
+		 */
+		System.out.println("For " + month + " months your acccumulated debt is " + amount + " . At a rate of " + rate + " will grow to " + (amount * Math.pow(1.0 + rate, month)));
+		 double rate, month, amount;
+		   
+		    
+		    System.out.println("Lets see the damage this time...");
+			Scanner keyboard = new Scanner(System.in);
+			
+		    System.out.println("How much debt have you accumulated?");
+		    amount = keyboard.nextDouble();
+		    
+		    System.out.println("What's your interest rate?");
+		    rate = keyboard.nextDouble();
+		    rate = rate/100;
+		    
+		    System.out.println("How many months?");
+		    month = keyboard.nextInt();
+		    
+	    keyboard.close();
+
+	} 
 	
-	System.out.println("For " + month + " months your acccumulated debt is " + amount + " . At a rate of " + rate + " will grow to " + (amount * Math.pow(1.0 + rate, month)));
-}
+	
 
 
 
