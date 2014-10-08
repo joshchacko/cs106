6eb591fa42b58005d413829ed78bc940cde9ad95
joshchacko <Josh@joshuas-mbp.skidmore.edu>
2014-10-08 18:42:19 -0400

    Finished!

--- Project 1/src/CCBA.java
+++ Project 1/src/CCBA.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 
 /**
- * @author jchacko
+ * @author Joshua Chacko
  *
  */
 public class CCBA {;
@@ -35,30 +35,8 @@ public class CCBA {;
 	    keyboard.close();
 	}
 }
-	public static void YoN(String[] args) {
-		/* I DID NOT COPY AND PASTE THIS!/* 
-		 * 
-		 */
-		System.out.println("For " + month + " months your acccumulated debt is " + amount + " . At a rate of " + rate + " will grow to " + (amount * Math.pow(1.0 + rate, month)));
-		 double rate, month, amount;
-		   
-		    
-		    System.out.println("Lets see the damage this time...");
-			Scanner keyboard = new Scanner(System.in);
-			
-		    System.out.println("How much debt have you accumulated?");
-		    amount = keyboard.nextDouble();
-		    
-		    System.out.println("What's your interest rate?");
-		    rate = keyboard.nextDouble();
-		    rate = rate/100;
-		    
-		    System.out.println("How many months?");
-		    month = keyboard.nextInt();
-		    
-	    keyboard.close();
+	
 
-	} 
 	
 	
 
