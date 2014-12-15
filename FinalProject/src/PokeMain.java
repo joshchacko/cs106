6d89bc3c8efbab4a0badad6c11cf64681aa50454
joshchacko <Josh@Joshuas-MacBook-Pro-2.local>
2014-12-15 11:55:43 -0500

    heh
    Looking good

--- FinalProject/src/PokeMain.java
+++ FinalProject/src/PokeMain.java
@@ -1,4 +1,24 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class PokeMain {
-
+	
+	public static void main(String[] args){
+		//lets you put multiple commands in one run
+		boolean running = true; 
+		Scanner keyboard = null;
+		try {
+			keyboard = new Scanner(new File(""));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(running){
+			String command = keyboard.nextLine();
+			//method call to PokeInput
+			if(command.equals("Stop")){
+				break;
+			}
+		}
+	
+	}
 }
