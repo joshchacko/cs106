a3abbb420db0543d2074e37114af4a5cad673800
joshchacko <Josh@Joshuas-MacBook-Pro-2.local>
2014-12-16 02:48:43 -0500

    tried

--- FinalProject/src/PokeMain.java
+++ FinalProject/src/PokeMain.java
@@ -1,24 +1,26 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.util.Scanner;
 public class PokeMain {
 	
+	/*
+	 * This will keep the program running, and propmt the user to input to the next line
+	 * "stop" will prompt a break, which will force terminate the program.S
+	 */
 	public static void main(String[] args){
 		//lets you put multiple commands in one run
 		boolean running = true; 
-		Scanner keyboard = null;
-		try {
-			keyboard = new Scanner(new File(""));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		Scanner keyboard = new Scanner(System.in);
+		
 		while(running){
+			System.out.println("What is your move?");
 			String command = keyboard.nextLine();
-			//method call to PokeInput
+			PokeInput input = new PokeInput(command);
 			if(command.equals("Stop")){
 				break;
-			}
+
 		}
 	
 	}
 }
+}
+	
